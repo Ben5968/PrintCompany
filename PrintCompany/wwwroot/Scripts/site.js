f7456dadbcc427ab240bd1869f85dee7d13dd2e5
Shad <Shad@SHADOW-60VV6STI>
2019-03-27 17:26:53 +0000

    CssAndSomeJsMovedToLayout

---
+++ PrintCompany/wwwroot/Scripts/site.js
@@ -0,0 +1,19 @@
+﻿$('form').validate(
+    {
+        errorPlacement: function (error, element) { },
+        highlight: function (element) {
+            $(element)
+                .closest('.form-group')
+                .addClass('has-error');
+        },
+        unhighlight: function (element) {
+            $(element)
+                .closest('.form-group')
+                .removeClass('has-error')
+        }
+    }
+);
+
+$('select').on('change', function () {
+    $(this).valid();
+});
